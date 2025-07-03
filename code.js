09c398b35ed79a04a3b72fca51a19957ac440bd8
Tashi Phuntsho <phuntsho931@gmail.com>
2025-07-03 12:32:15 +0600

    er

--- code.js
+++ code.js
@@ -14,14 +14,3 @@ function paperwork(n, m) {
 }
 console.log(paperwork(2,10))
 
-function betterThanAverage(classpoints, yourpoints){
-    let tot = 0;
-    for(let i = 0; i < classpoints.length; i++){
-        tot += classpoints[i]
-    }
-    let avg = tot/classpoints.length;
-    return yourpoints > avg
-        
-    
-}
-console.log(betterThanAverage([20,50,60,40,70],90))
